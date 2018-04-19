2e4ff66d2e4b6538d934a19345620d48e11b6756
fish_study <fish_study@126.com>
2018-04-19 23:10:54 +0800

    learn

--- note/http 学习笔记.go
+++ note/http 学习笔记.go
@@ -6,3 +6,8 @@
  html/template  模版库
 
  template.New("模版名称", "文件解析")
+
+ template.ParseFiles方法加载模板
+ template.Execute 加载模版的内容
+ template.Must 检测模版是否正确
+ 对于go模板，with语句类似，其含义就是创建一个封闭的作用域，在其范围内，可以使用.action，而与外面的.无关，只与with的参数有关：
